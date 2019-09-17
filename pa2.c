58f01165cf12a5c012e3c8b6a14ddea94971b285
varun997 <varun.g@gmail.com>
2019-09-17 22:32:58 +0530

    updated

--- pa2.c
+++ pa2.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main(int argc, char * argv[])
 {
-  int n;  
-  n= atoi(argv[1]); \\ no of people
-  char names[n][100]; char expen[100][100]
+  int n; int i; 
+  n= atoi(argv[1]); /* no of people */
+  char names[n][100];
+  char expen[100][100];
   for(i=0;i<n;i++)
    {
-     scanf("%s", &(names[i][0]));    \\ Inputting the names of the members 
+     scanf("%s", &(names[i][0]));    /* Inputting the names of the members */ 
    }
   for(i=0;i<100;i++)
    {
-     scanf("%s" ,&(expen[i][0]);
-     if( (expen[i][0]) == "done" )
+     scanf("%s" ,&(expen[i][0]));      /* Inputting the expenses */
+     if(strcmp((&(expen[i][0])),"done") == 0) /* condition if 'done' is inputted */   
      break;
-   } 
+   }
+  for(i=0;i
+   
+}
+
